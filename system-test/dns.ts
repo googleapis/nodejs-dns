e902cd254f7ac1c614633548a4ecb22b14b7a8af
Justin Beckwith <beckwith@google.com>
2020-04-14 14:47:09 -0700

    come on now

--- system-test/dns.ts
+++ system-test/dns.ts
@@ -125,7 +125,7 @@ describe('dns', () => {
   });
 
   // deal with eventual consistency of ZONE.create():
-  it('should return 0 or more zones', async function() {
+  it('should return 0 or more zones', async function () {
     this.retries(3);
     await delayMs(1000);
     const zones = await dns.getZones();
